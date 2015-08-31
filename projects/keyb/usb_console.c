620f634ce855ba25f0fe8ecfc66678f2622c822e
flabbergast <s3+flabbergast@sdfeu.org>
2015-08-31 11:35:30 +0100

    Reorganize files in keyb.

--- projects/keyb/usb_console.c
+++
@@ -1,8 +0,0 @@
-#include "usb_console.h"
-
-// debug IN callback hander
-static void console_in_cb(USBDriver* usbp, usbep_t ep) {
-  // STUB
-  (void)usbp;
-  (void)ep;
-}
--- projects/keyb/usb_mouse.c
+++
@@ -1,7 +0,0 @@
-
-// mouse IN callback hander
-static void mouse_in_cb(USBDriver* usbp, usbep_t ep) {
-  // STUB
-  (void)usbp;
-  (void)ep;
-}
